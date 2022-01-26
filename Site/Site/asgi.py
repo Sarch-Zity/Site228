06bc0429ab05064c1e3a2b85d6a0e236d0dc8003
Sarch-Zity <artem.zity@gmail.com>
2022-01-26 20:08:41 +0700

    0.2

--- Site/Site/asgi.py
+++ Site/Site/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for realniysayt project.
+ASGI config for Site project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'realniysayt.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Site.settings')
 
 application = get_asgi_application()
